ad6cd58282a9925593c0b0450acf280340333628
Eldemka <el.demm11@gmail.com>
2024-11-15 11:44:50 +0300

    second commit

--- src/P02/P01.java
+++ src/P02/P01.java
@@ -1,10 +1,22 @@
 package P02;
 
+import java.util.Scanner;
+
 public class P01 {
     public static void main(String[] args) {
 
         int a=128;
         int b=127;
         System.out.println("c="+(a+b));
+
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println(" Dört harfli bir kelime griniz");
+        String kelime= scan.nextLine();
+
+        if(kelime.length()==4){
+            System.out.println("giridğiniz kelime geçersizdir");
+        }
+
     }
 }
