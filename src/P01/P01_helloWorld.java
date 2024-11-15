277ee0bafcc2a6e58f3fd8165512675b5720183a
Eldemka <el.demm11@gmail.com>
2024-11-15 11:40:23 +0300

    first commit

---
+++ src/P01/P01_helloWorld.java
@@ -0,0 +1,9 @@
+package P01;
+
+public class P01_helloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("hello world");
+    }
+}
---
+++ src/P02/P01.java
@@ -0,0 +1,10 @@
+package P02;
+
+public class P01 {
+    public static void main(String[] args) {
+
+        int a=128;
+        int b=127;
+        System.out.println("c="+(a+b));
+    }
+}
